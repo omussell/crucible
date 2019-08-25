935243a9667225ff61d7ac50860da764f60749e9
omussell <oliver.mussell@hotmail.co.uk>
2019-08-25 19:53:43 +0100

    Update with some more functions. Template added but doesnt work yet

--- make_manifest.py
+++ make_manifest.py
@@ -1,22 +1,44 @@
-def generate_deps_info(deps: list):
-    for dep in deps:
-        pkg_info = subprocess.check_output(["pkg", "search", "-e", "-S", "name", "-Q", "full", dep])
-        origins_info = [line for line in pkg_info.decode('UTF-8').split('\n') if 'Origin' in line]
-        for origin in origins_info:
-            print(origin.split(':')[1].strip())
-        versions_info = [line for line in pkg_info.decode('UTF-8').split('\n') if 'Version' in line]
-        for version in versions_info:
-            print(version.split(':')[1].strip())
+import subprocess
 
-with open('META_DEPENDENCIES', 'r') as md:
-    meta_deps = [dep.rstrip('\n') for dep in md]
-    generate_deps_info(meta_deps)
+#def get_pkg_info(dep: str):
+#    pkg_info = subprocess.check_output(["pkg", "search", "-e", "-S", "name", "-Q", "full", dep])
+
+def generate_dep_origin(dep: str):
+    pkg_info = subprocess.check_output(["pkg", "search", "-e", "-S", "name", "-Q", "full", dep])
+    origins_info = [line for line in pkg_info.decode('UTF-8').split('\n') if 'Origin' in line]
+    for origin in origins_info:
+        deps_origin = origin.split(':')[1].strip()
+    return deps_origin
+
+def generate_dep_version(dep: str):
+    pkg_info = subprocess.check_output(["pkg", "search", "-e", "-S", "name", "-Q", "full", dep])
+    versions_info = [line for line in pkg_info.decode('UTF-8').split('\n') if 'Version' in line]
+    for version in versions_info:
+        deps_version = version.split(':')[1].strip()
+    return deps_version
+
+deps_dict = {}
+for dep in deps:
+    deps_dict[dep] = {}
+    deps_dict[dep]['origin'] = generate_dep_origin(dep)
+    deps_dict[dep]['version'] = generate_dep_version(dep)
+
+template.render()
 
 
 
-import jinja2
+deps = ['tmux']
+generate_deps_info(deps)
 
-template_loader = jinja2.FileSystemLoader(searchpath="templates")
-template_env = jinja2.Environment(loader=template_loader)
-template = template_env.get_template('filename')
-outputText = template.render()
+#with open('META_DEPENDENCIES', 'r') as md:
+#    meta_deps = [dep.rstrip('\n') for dep in md]
+#    generate_deps_info(meta_deps)
+#
+#
+#
+#import jinja2
+#
+#template_loader = jinja2.FileSystemLoader(searchpath="templates")
+#template_env = jinja2.Environment(loader=template_loader)
+#template = template_env.get_template('filename')
+#outputText = template.render()
