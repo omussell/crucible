8c55ee28988f55205fe162819aa6f99e89ab352a
Oliver Mussell <oliver.mussell@primarysite.net>
2020-04-24 13:56:56 +0100

    Add nginx to init

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -4,9 +4,11 @@ import fabfile.jail
 import fabfile.zfs
 import fabfile.pkg
 import fabfile.templates
+import fabfile.nginx
 
 ns = Collection()
 ns.add_collection(jail)
 ns.add_collection(zfs)
 ns.add_collection(pkg)
 ns.add_collection(templates)
+ns.add_collection(nginx)
